a0cd2f47bd6c0499c70966259a37bc6a2447f81f
gokulnathanT <mailtogokul1727@gmail.com>
2024-09-02 13:36:41 +0530

    bug fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
                 variants={container(0)}
                 initial="hidden"
                 animate="visible"
-                className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Gokulnathan Thanapal</motion.h1>
+                className="pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Gokulnathan Thanapal</motion.h1>
                 <motion.span  variants={container(0.5)}
                 initial="hidden"
                 animate="visible"
