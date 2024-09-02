8251782d7c0aaa25f55cb2b0a9e40a900d744fcb
gokulnathanT <mailtogokul1727@gmail.com>
2024-09-02 13:40:25 +0530

    bug fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
         <div className="flex flex-shrink-0 items-center">
             <img className="mx-2 w-10" src={logo} alt="logo" />
         </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <div className="m-8 flex items-center justify-center gap-4 text-1xl">
             <a href="https://www.linkedin.com/in/gokulnathan-thanapal-815586259/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
             <a href="https://github.com/gokulnathanT" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
             <a href="mailto:mailtogokul1727@gmail.com" target="_blank" rel="noopener noreferrer"><FaEnvelope/></a>
